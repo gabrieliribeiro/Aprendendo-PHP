147f42d105f4d9f6a75daa35b105649232408f23
gabrieliribeiro <gabrielirt@hotmail.com>
2022-05-24 17:21:39 -0300

    Enviando alterações e atualizações para o programa

--- strings-inicial/autoload.php
+++ strings-inicial/autoload.php
@@ -9,12 +9,14 @@ spl_autoload_register(function ($classe){
         return;
     }
 
-    $arquivo = $diretorio;
-
     $namespace = substr($classe, strlen($prefixo));
 
     $namespace_arquivo = str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
 
     $arquivo = $diretorio . $namespace_arquivo . '.php';
+
+    if (file_exists($arquivo)) {
+        require $arquivo;
+    }
 
 });
